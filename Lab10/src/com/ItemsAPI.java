38bc4fbadba4257904f323340b0a6695ce400114
thami <thami@DESKTOP-LQJ1807>
2020-05-03 13:03:07 +0530

    Implement  getParasMap() and doPut() methods
    getParasMap() to Convert request parameters to a Map
    doPut() to Update

--- Lab10/src/com/ItemsAPI.java
+++ Lab10/src/com/ItemsAPI.java
@@ -24,23 +24,17 @@ public class ItemsAPI extends HttpServlet {
        
     public ItemsAPI() {
         super();
-        // TODO Auto-generated constructor stub
+
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//doGet(request, response);
 		
 		String output = itemObj.insertItem(request.getParameter("itemCode"),
 							request.getParameter("itemName"),
@@ -49,19 +43,54 @@ public class ItemsAPI extends HttpServlet {
 		response.getWriter().write(output);
 		
 	}
-
-	/**
-	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
-	 */
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	
+	
+	// Convert request parameters to a Map
+	private static Map getParasMap(HttpServletRequest request) {
+		
+		Map<String, String> map = new HashMap<String, String>();
+		
+		try {
+			
+			Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+			String queryString = scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
+			
+			scanner.close();
+			
+			String[] params = queryString.split("%");
+			
+			for (String param : params) {
+				
+				String[] p = param.split("=");
+				map.put(p[0], p[1]);
+				
+			}
+			
+			
+		} catch (Exception e) {
+			// Handle exception
+		}
+		
+		return map;
 	}
 
-	/**
-	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
-	 */
+	
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Map paras = getParasMap(request);
+		
+		String output = itemObj.updateItem(paras.get("hidItemIDSave").toString(),
+				paras.get("itemCode").toString(),
+				paras.get("itemName").toString(),
+				paras.get("itemPrice").toString(),
+				paras.get("itemDesc").toString());
+		response.getWriter().write(output);
+		
+	}
+
+
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 	}
 
 }

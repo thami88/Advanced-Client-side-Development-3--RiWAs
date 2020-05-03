74051c6146b97c7231f7b4b85f67ff5306eaf104
thami <thami@DESKTOP-LQJ1807>
2020-05-03 14:30:30 +0530

    Implement the complete function onItemSaveComplete()

--- Lab10/WebContent/Components/items.js
+++ Lab10/WebContent/Components/items.js
@@ -38,6 +38,43 @@ $(document).on("click", "#btnSave", function(event) {
 	
 });
 
+function onItemSaveComplete(response, status){
+	
+	if (status == "success") {
+		
+		var resultSet = JSON.parse(response);
+		
+		if (resultSet.status.trim() == "success") {
+			
+			$("#alertSuccess").text("Successfully saved.");
+			$("#alertSuccess").show();
+			
+			$("#divItemsGrid").html(resultSet.data);
+			
+		} else if (resultSet.status.trim() == "error") {
+			
+			$("#alertError").text(resultSet.data);
+			$("#alertError").show(); 
+
+		}
+		
+	} else if (status == "error") {
+		
+		$("#alertError").text("Error while saving.");
+		$("#alertError").show();
+
+	} else {
+		
+		$("#alertError").text("Unknown error while saving..");
+		$("#alertError").show(); 
+		
+	}
+	
+	$("#hidItemIDSave").val("");
+	$("#formItem")[0].reset(); 
+	
+}
+
 // UPDATE==========================================
 $(document).on("click", ".btnUpdate", function(event) 
 		{

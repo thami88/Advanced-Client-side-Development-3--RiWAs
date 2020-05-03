505dafd60c1655ff822ce3b6eb20c75d19b85cc3
thami <thami@DESKTOP-LQJ1807>
2020-05-03 12:35:39 +0530

    Implenet doPost() for Insert operation
     Create object from Item class

--- Lab10/src/com/ItemsAPI.java
+++ Lab10/src/com/ItemsAPI.java
@@ -1,22 +1,27 @@
 package com;
 
+import com.Item;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 
 /**
  * Servlet implementation class ItemsAPI
  */
 @WebServlet("/ItemsAPI")
 public class ItemsAPI extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
+	
+	Item itemObj = new Item();
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public ItemsAPI() {
         super();
         // TODO Auto-generated constructor stub
@@ -35,7 +40,14 @@ public class ItemsAPI extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//doGet(request, response);
+		
+		String output = itemObj.insertItem(request.getParameter("itemCode"),
+							request.getParameter("itemName"),
+							request.getParameter("itemPrice"),
+							request.getParameter("itemDesc"));
+		response.getWriter().write(output);
+		
 	}
 
 	/**

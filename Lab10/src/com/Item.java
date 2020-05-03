39440d1da1f5a1f1b11576bb17ff5af4b1278763
thami <thami@DESKTOP-LQJ1807>
2020-05-03 10:40:59 +0530

    Implement DB connection Method in Item Class

--- Lab10/src/com/Item.java
+++ Lab10/src/com/Item.java
@@ -1,5 +1,27 @@
 package com;
 
+import java.sql.*; 
+
 public class Item {
+	
+	// Method to Connect Database /------------------------------------------------------------
+	
+	private Connection connect() {
+		
+		Connection con = null; 
+		
+		try {
+			
+			Class.forName("com.mysql.jdbc.Driver"); 
+			con = DriverManager.getConnection("jdbc:mysql://localhost/item_management", "root", "");
+			
+		} catch (Exception e) {
+			// Handle exception
+			e.printStackTrace();
+		}
+		
+		return con;
+		
+	}
 
 }
